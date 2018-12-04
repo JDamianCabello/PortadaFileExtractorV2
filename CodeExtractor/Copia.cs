49623d9b021787358126c3f69d942c6672f4cc18
Damián Cabello <jdamiancabello@gmail.cm>
2018-12-04 23:39:56 +0100

    First functional beta V 0.1

---
+++ CodeExtractor/Copia.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace CodeExtractor
+{
+    static class Copy
+    {
+
+        /// <summary>
+        /// Copy expecific files filtering by extension
+        /// </summary>
+        /// <param name="sourcePath">Original full directory patch</param>
+        /// <param name="destPath">Destination full directory patch</param>
+        /// <param name="cs">search all *.cs extensions in the directory</param>
+        /// <param name="exe">search all *.exe extensions in the directory</param>
+        /// <param name="dll">search all *.dll extensions in the directory</param>
+        /// <param name="compress">Determines if the folder is compressed</param>
+        /// <param name="deleteAfterCompress">Determines if should delete the compress directory after zip</param>
+        public static void ExpecificFiles(string sourcePath, string destPath, bool cs, bool exe, bool dll, bool compress, bool deleteAfterCompress)
+        {
+
+            string[] directorys = Directory.GetDirectories(sourcePath);
+            string[] filesToCopy = null;
+            string[] extensionsToCopy = CreateExtensionList(cs, exe, dll);
+
+            for (int i = 0; i < directorys.Length; i++)
+            {
+                filesToCopy = SearchFilesToCopy(directorys[i], extensionsToCopy);
+                if (!Directory.Exists(DirectoryEndPatch(directorys[i], destPath )))
+                    Directory.CreateDirectory(DirectoryEndPatch(directorys[i], destPath));
+
+                for (int j = 0; j < filesToCopy.Length; j++)
+                    File.Copy(filesToCopy[j], FileEndPatch(directorys[i], destPath, Path.GetFileName(filesToCopy[j])), true);
+            }
+            if(compress)
+                Compress(destPath, Path.GetFileName(destPath), deleteAfterCompress);
+        }
+
+        /// <summary>
+        /// Method that makes a zip from entire folder
+        /// </summary>
+        /// <param name="path">Patch to make the zip</param>
+        /// <param name="fileName">Zip name</param>
+        /// <param name="deleteDirectoryAfterZip">Determines if should delete the compress directory after zip</param>
+        private static void Compress (string path, string fileName, bool deleteDirectoryAfterZip)
+        {
+            string rutaDirectorioPadre = new DirectoryInfo(path).Parent.FullName + Path.DirectorySeparatorChar;
+            ZipFile.CreateFromDirectory(path, rutaDirectorioPadre + fileName + ".zip",CompressionLevel.Optimal,false);
+            if(deleteDirectoryAfterZip)
+                Directory.Delete(path, true);
+        }
+
+        /// <summary>
+        /// Aux method. Make a end directory patch from source patch and destination patch
+        /// </summary>
+        /// <param name="sourcePath">Original full directory patch</param>
+        /// <param name="destPath">Destination full directory patch</param>
+        /// <returns>The end directory pach</returns>
+        private static string DirectoryEndPatch(string sourcePath, string destPath)
+        {
+            return destPath + Path.DirectorySeparatorChar + Path.GetFileName(sourcePath);
+        }
+
+        /// <summary>
+        /// Aux method. Make a end file patch from source patch and destination patch
+        /// </summary>
+        /// <param name="sourcePath">Original full file patch</param>
+        /// <param name="destPath">Destination full file patch</param>
+        /// <param name="fileName">File name and file extesion</param>
+        /// <returns>The end file patch</returns>
+        private static string FileEndPatch(string sourcePath, string destPath, string fileName)
+        {
+            return destPath + Path.DirectorySeparatorChar + Path.GetFileName(sourcePath) + Path.DirectorySeparatorChar + fileName;
+        }
+
+        /// <summary>
+        /// Search all files in folder by extension and exludes directorys or files
+        /// </summary>
+        /// <param name="folderPath">Full patch to father folder</param>
+        /// <param name="extension">Extension to search in father folder</param>
+        /// <returns></returns>
+        private static string[] SearchFilesToCopy(string folderPath, string[] extension)
+        {
+            List<string> files = new List<string>();
+            string[] excludesPatterns = { "\\obj\\Debug\\", "\\Properties\\", ".vshost." };
+            for (int i = 0; i < extension.Length; i++)
+            {
+                string[] filePaths = Directory.GetFiles(folderPath, extension[i],SearchOption.AllDirectories); //Search in the folder and subfolders
+                for (int j = 0; j < filePaths.Length; j++)
+                    if (!ExcludedFilesOrFolders(filePaths[j], excludesPatterns))
+                        files.Add(filePaths[j]); 
+            }
+
+            return files.ToArray();
+        }
+        /// <summary>
+        /// Check if these file or folder should be excluded from copy
+        /// </summary>
+        /// <param name="patch">Patch to file</param>
+        /// <param name="excludedPatterns">All excludes patterns</param>
+        /// <returns>A list of patch´s to copy</returns>
+        private static bool ExcludedFilesOrFolders(string patch, string[] excludedPatterns)
+        {
+            for (int i = 0; i < excludedPatterns.Length; i++)
+                if (patch.Contains(excludedPatterns[i]))
+                    return true;
+
+            return false;
+
+
+        }
+
+        /// <summary>
+        /// Aux method for save extensions to search.
+        /// </summary>
+        /// <param name="cs">search all *.cs extensions in the directory</param>
+        /// <param name="exe">search all *.exe extensions in the directory</param>
+        /// <param name="dll">search all *.dll extensions in the directory</param>
+        /// <returns>A list of extensions to search</returns>
+        private static string[] CreateExtensionList(bool cs, bool exe, bool dll)
+        {
+            List<string> auxList = new List<string>();
+
+            if (cs)
+                auxList.Add("*.cs");
+            if (exe)
+                auxList.Add("*.exe");
+            if (dll)
+                auxList.Add("*.dll");
+
+            return auxList.ToArray();
+        }
+    }
+
+
+}
---
+++ CodeExtractor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalPatch, destinationPatch;
+            Console.WriteLine("Original patch:");
+            originalPatch = Console.ReadLine();
+            Console.WriteLine("Destination patch:");
+            destinationPatch = Console.ReadLine();
+
+            Copy.ExpecificFiles(originalPatch, destinationPatch, true,true,true,true,false);
+
+            Console.WriteLine("All done, press one key to leave.");
+            Console.ReadKey();
+        }
+    }
+}
